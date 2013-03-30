2f468ef239cfc83c15b14b3d6e344e67c3480aab
Chris Dillon <squarism@gmail.com>
2013-03-30 13:46:37 -0400

    DOS newlines on Rakefile, unsupported rake/rdoctask fix, other minor typos

--- lib/array_stats.rb
+++ lib/array_stats.rb
@@ -2,7 +2,7 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module ArrayStats
-  VERSION = '0.5.0'
+  VERSION = '0.6.0'
 end
 
 require "#{File.dirname(__FILE__)}/array_stats/float"
